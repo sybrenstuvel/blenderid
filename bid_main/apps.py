e0c0be9e1f94efb108c53fa0eb534fbc6f524b11
Sybren A. Stüvel <sybren@stuvel.eu>
2016-10-10 18:47:02 +0200

    Deleted signal handlers, as we don't need them any more.

--- bid_main/apps.py
+++ bid_main/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class BidMainConfig(AppConfig):
     name = 'bid_main'
     verbose_name = 'Blender-ID'
-
-    def ready(self):
-        # Just import for the side-effects.
-        from . import signals
-        assert signals
