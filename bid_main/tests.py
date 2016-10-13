def39e2daf3867d0dbdce3594b53f2e9da5aa746
Sybren A. Stüvel <sybren@stuvel.eu>
2016-10-13 21:02:49 +0200

    Fixed test, and removed now-obsolete test

--- bid_main/tests.py
+++ bid_main/tests.py
@@ -2,39 +2,14 @@ from django.test import TestCase
 
 
 class AutoCreateUserProfileTest(TestCase):
-
     def test_create_user_happy_flow(self):
-        from django.contrib.auth.models import User
+        from django.contrib.auth import get_user_model
 
-        user = User(username='example',
-                    email='example@example.com',
-                    first_name='Dr.',
-                    last_name='Examplović',
-                    )
+        user_cls = get_user_model()
+        user = user_cls(email='example@example.com',
+                        full_name='Dr. Examplović')
         user.save()
 
-        self.assertIsNotNone(user.profile)
-        self.assertEqual('Dr. Examplović', user.profile.full_name)
+        self.assertEqual('Dr. Examplović', user.get_full_name())
 
         return user
-
-    def test_user_change_profile_name(self):
-        user = self.test_create_user_happy_flow()
-
-        user.first_name = ''
-        user.last_name = ''
-        user.save()
-
-        self.assertEqual('Dr. Examplović', user.profile.full_name)
-
-        user.profile.full_name = 'ဟင်းချို'
-        user.profile.save()
-        self.assertEqual('ဟင်းချို', user.profile.full_name)
-        self.assertEqual('ဟင်းချို', user.first_name)
-        self.assertEqual('', user.last_name)
-
-        user.profile.full_name = 'dr. Sybren A. Stüvel'
-        user.profile.save()
-        self.assertEqual('dr. Sybren A. Stüvel', user.profile.full_name)
-        self.assertEqual('dr. Sybren', user.first_name)
-        self.assertEqual('A. Stüvel', user.last_name)
