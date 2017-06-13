167f77639e011195c8c1d14aab37a5d2319d9edc
Sybren A. Stüvel <sybren@stuvel.eu>
2017-06-13 17:15:21 +0200

    Made static files work with the dev server

--- blenderid/common_settings.py
+++ blenderid/common_settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import pathlib
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,9 +122,9 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [pathlib.Path(BASE_DIR) / 'static']
 
 BLENDER_ID_ADDON_CLIENT_ID = '-secret-'
 
