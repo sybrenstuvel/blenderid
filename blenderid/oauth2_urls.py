04a8146eb5a0d7d7076124578a903ece54aa06ce
Sybren A. Stüvel <sybren@stuvel.eu>
2017-11-14 11:01:25 +0100

    Fixed revocation URL

--- blenderid/oauth2_urls.py
+++ blenderid/oauth2_urls.py
@@ -13,5 +13,5 @@ from oauth2_provider import views
 urlpatterns = (
     url(r'^authorize/?$', views.AuthorizationView.as_view(), name="authorize"),
     url(r'^token/?$', views.TokenView.as_view(), name="token"),
-    url(r'^revoke_token/?$', views.RevokeTokenView.as_view(), name="revoke-token"),
+    url(r'^revoke/?$', views.RevokeTokenView.as_view(), name="revoke-token"),
 )
