3180ddd6178bbb91931e3705aba06b81c5d565c0
Sybren A. Stüvel <sybren@stuvel.eu>
2017-11-17 16:27:28 +0100

    'migrate_olddb' now also migrates OAuth2 clients

--- bid_main/management/commands/migrate_olddb.py
+++ bid_main/management/commands/migrate_olddb.py
@@ -64,6 +64,7 @@ class Command(BaseCommand):
         self.migrate_users(*args, **options)
         self.migrate_user_roles()
         self.migrate_user_settings()
+        self.migrate_oauth_clients()
 
     @transaction.atomic()
     def migrate_roles(self):
@@ -181,3 +182,58 @@ class Command(BaseCommand):
             cursor.execute('insert ignore into bid_main_usersetting (id, user_id, setting_id, unconstrained_value) '
                            'select id, user_id, setting_id, unconstrained_value from users_settings')
         self.stdout.write(self.style.SUCCESS('Migrated user-settings'))
+
+    @transaction.atomic()
+    def migrate_oauth_clients(self):
+        from oauth2_provider.models import get_application_model
+        from django.contrib.auth import get_user_model
+
+        app_model = get_application_model()
+        user_model = get_user_model()
+
+        # Old database:
+        # +---------------------------+------------------------------------------------------+--------------------------------------+------------------------------------------+-----------------------------------------------------+---------+--------------------------------------------------------------------------------------------------------------------------------------+-----------------+-------------------------------------+
+        # | name                      | description                                          | picture                              | client_id                                | client_secret                                       | user_id | _redirect_uris                                                                                                                       | _default_scopes | url                                 |
+        # +---------------------------+------------------------------------------------------+--------------------------------------+------------------------------------------+-----------------------------------------------------+---------+--------------------------------------------------------------------------------------------------------------------------------------+-----------------+-------------------------------------+
+        # | Frameshift                | NULL                                                 | NULL                                 | asgiuehg90w09h4ingf8236yhfb9ijgtujdetghf | g4e6d78iujtf0vs9iuhws5rtfvrf9oiktg0okgghnpokjdfdsdf |   39055 | https://api.frameshift.studio/auth/login                                                                                             | email           | NULL                                |
+        # | Blender Cloud             | Blender Cloud, by Blender Institute                  | blender_cloud.jpg                    | BLENDER-CLOUD-CLIENT                     | aerouthtcioiyrrorotenntzisnhatEa                    |       7 | https://cloud.blender.org/oauth/blender-id/authorized                                                                                | email           | NULL                                |
+        # | Blender Conference        | The Blender event of the year.                       | fe4217352d0e73d7dee87ee8f93ad59c.jpg | BLENDER-CONFERENCE                       | asfkjhoigoeoihgoailjsagxcvaavvd                     |       7 | https://www.blender.org/conference/oauth/authorized http://www.blender.org/conference/oauth/authorized                               | email           | https://www.blender.org/conference  |
+        # | Blender Conference DEV    | The Blender event of the year.                       | fe4217352d0e73d7dee87ee8f93ad59c.jpg | BLENDER-CONFERENCE-DEV                   | sosecret                                            |       7 | http://localhost:5000/oauth/authorized                                                                                               | email           | https://www.blender.org/conference  |
+        # | Blender ID addon          | NULL                                                 | NULL                                 | BLENDER-ID-ADDON-CLIENT                  | aeaOEdrolopsJsDdanrnrnairarcmtnr                    |       7 |                                                                                                                                      | email           | NULL                                |
+        # | Dalai Felinto             | Dalai's Adventures                                   | 2d94d846ba210a0e9c034194a666cdf6.jpg | DALAI-SECRET-ONLY                        | 8tNjZa0ZUHnwAY4fsaskjfh98yrh23jbkwfq98y3fh83        |    1522 | https://flask.dalaifelinto.com/callback/blender                                                                                      | email           | https://flask.dalaifelinto.com/     |
+        # | Graphicall                | Bringing open source builds to the masses since 2005 | 88b4fe81313170a7e36bf607de816396.jpg | GRAPHICALL                               | graphicallsecred                                    |       7 | http://graphicall.org/login/                                                                                                         | email           | http://graphicall.org/about         |
+        # | Rightclick Select         | NULL                                                 | NULL                                 | qkDocF9wc0Bc4pxnF3dcY4BJVa8up37A53TbbnmS | x23rqTg9r6izOQnl2UJEHbJ8DJgI8h6Y9AR33im3HB5bI8joci  |    8582 | https://rightclickselect.com/authorized                                                                                              | email           | http://rightclickselect.com/        |
+        # | Blender Network Local     | Local blender network                                | NULL                                 | qkDocF9wc0Bc4pxnF3dcY4BJVa8up37QUdTMenmS | x23rqTg9r6izOQnl2UJEHbJ8DJgI8Wt0k3R33im3HB5bI8joci  |       7 | http://network.blender.org:5000/authorized                                                                                           | email           | NULL                                |
+        # | Blender Today             | Community-driven Blender News                        | ab531849f8738e363eeaf089605ee999.png | sZ0fJCyYslqjWParf9exH5LMmToMXqESx92gu2dd | aVrmd0tXLwA3m6Mhlpsxc0DfOMUcRVkK6YL8Ngs5xFuCawS1fP  |       7 | http://blender.today/oauth/blender-id/authorized                                                                                     | email           | http://blender.today                |
+        # | Blender Community         | Blender Community                                    | a04a5bce210bfb21251bd2f6af56b565.jpg | sZ0fJCyYslqjWParf9exH5LMmToMXqESx92gu2E6 | aVrmd0tXLwA3m6Mhlpsxc0DfOMUcRVkK6YL8Ngs5xFuCawS1fs  |       7 | https://blender.community/oauth/blender-id/authorized                                                                                | email           | https://blender.community           |
+        # | Blender Network Staging   | The Blender Network website                          | ab531849f8738e363eeaf089605ee950.png | sZ0fJCyYslqjWParf9exH5LMmToMXqESx92gu2Ef | aVrmd0tXLwA3m6Mhlpsxc0DfOMUcRVkK6YL8Ngs5xFuCawS1fK  |       7 | https://staging.blendernetwork.org/authorized http://staging.blendernetwork.org/authorized https://www.blendernetwork.org/authorized | email           | https://www.blendernetwork.org      |
+        # | Blender Network           | The Blender Network website                          | 451fd5e3aec35f94854112c0a0dbdd7d.png | sZ0fJCyYslqjWParf9exH5LMmToMXqESx92gu2Ej | aVrmd0tXLwA3m6Mhlpsxc0DfOMUcRVkK6YL8Ngs5xFuCawS1fP  |       7 | http://www.blendernetwork.org/authorized https://www.blendernetwork.org/authorized                                                   | email           | https://www.blendernetwork.org      |
+        # | Blender Network Community | The Blender Network discussion platform              | 451fd5e3aec35f94854112c0a0dbdd7d.png | sZ0fJCyYslqjWParf9exH5LMmToMXqESx92gu2Es | aVrmd0tXLwA3m6Mhlpsxc0DfOMUcRVkK6YL8Ngs5xFuCawS1fs  |       7 | https://community.blendernetwork.org/oauth/blender-id/authorized                                                                     | email           | http://community.blendernetwork.org |
+        # | HDRI Planet               | NULL                                                 | NULL                                 | u0XuWtbo7ahMl0FmA9FBCkilndLdlG9udRJmRRpA | hNhaHN9HDokKGTjOCSMR0UFeZc2NKnPzEjCGn6BG            |   29973 | http://hdriplanet.com/oauth/blender-id/auth                                                                                          | email           | http://hdriplanet.com               |
+        # | HDRI Planet Local         | NULL                                                 | NULL                                 | x5ZPLQrMURtFnx801H0cyOg52H0cVSf2CHlEKnzX | vcRWSFbfoeGjabQo5NtlXFRgbQQQ7gE1zn24HDtF            |   29973 | http://hdriplanet.localhost/oauth/blender-id/auth                                                                                    | email           | http://hdriplanet.localhost         |
+        # | Render Street             | Render Street                                        | NULL                                 | YspImpg18chyGZVzkhUwDdcfPiFynXQUIt003r7  | 8tNjZa0ZUHnwAY4m1wnmkMeRBhsqSnlCNHx8FFU             |   12322 | https://render.st/oauth/blender-id/auth                                                                                              | email           | https://render.st                   |
+        # +---------------------------+------------------------------------------------------+--------------------------------------+------------------------------------------+-----------------------------------------------------+---------+--------------------------------------------------------------------------------------------------------------------------------------+-----------------+-------------------------------------+
+
+        migrated = 0
+
+        # Only query for clients that don't exist yet.
+        for result in query("SELECT name, client_id, client_secret, _redirect_uris as redirect_uris, user_id, url FROM client WHERE client_id not in "
+                            "(SELECT client_id FROM %s)" % app_model._meta.db_table):
+            user = user_model.objects.get(id=result.user_id)
+            is_bid_addon = result.client_id == 'BLENDER-ID-ADDON-CLIENT'
+
+            app = app_model(
+                authorization_grant_type='password' if is_bid_addon else 'authorization-code',
+                client_id=result.client_id,
+                client_secret=result.client_secret,
+                client_type='confidential',
+                name=result.name,
+                redirect_uris=result.redirect_uris,
+                skip_authorization=False,
+                url=result.url or '',
+                user=user,
+            )
+            app.save()
+            migrated += 1
+
+        self.stdout.write(self.style.SUCCESS('Migrated %d OAuth2 applications' % migrated))
